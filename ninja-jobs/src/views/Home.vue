bad1e1459defd9900ae63e1e87f518033a654032
Ted Irland <irlandth@gmail.com>
2021-06-03 10:19:43 -0500

    Removed HelloWorld component, added some mockup text to evaluate design

--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita enim nihil nulla quasi sit, aspernatur inventore magni, quas provident nam eos ducimus, optio id esse culpa facilis aut voluptatem maxime tempore. Magni nulla commodi temporibus neque odit similique asperiores. Unde facilis nesciunt facere voluptatibus cumque necessitatibus ea illum quaerat voluptas saepe, provident laboriosam ex cupiditate at esse quibusdam nemo aspernatur quos fugit sapiente, a animi. Nihil labore, commodi voluptatibus ea sunt minima vero rerum dolor saepe harum impedit tempora fuga voluptatum soluta veniam! Molestiae aperiam itaque ipsa. Expedita maiores quos minus, magni, facere incidunt placeat molestiae, quia consequuntur ut atque?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita enim nihil nulla quasi sit, aspernatur inventore magni, quas provident nam eos ducimus, optio id esse culpa facilis aut voluptatem maxime tempore. Magni nulla commodi temporibus neque odit similique asperiores. Unde facilis nesciunt facere voluptatibus cumque necessitatibus ea illum quaerat voluptas saepe, provident laboriosam ex cupiditate at esse quibusdam nemo aspernatur quos fugit sapiente, a animi. Nihil labore, commodi voluptatibus ea sunt minima vero rerum dolor saepe harum impedit tempora fuga voluptatum soluta veniam! Molestiae aperiam itaque ipsa. Expedita maiores quos minus, magni, facere incidunt placeat molestiae, quia consequuntur ut atque?</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nostrum animi est exercitationem iste, eum ab voluptates minus autem cupiditate temporibus fuga laboriosam accusantium ipsum natus dolor labore molestiae excepturi.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
