291a102fa35261c46275748732cc4e3d46d54c03
Ted Irland <irlandth@gmail.com>
2021-06-04 08:58:25 -0500

    Fetched data from json server and wrote fetch logic in the mounted hook of each component

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,16 +1,28 @@
 <template>
   <h1>Job details page</h1>
-  <p>The job id is {{id}}</p>
+  <div v-if="job">
+  <p>Title: {{job.title}}</p>
+  <p>Details: {{job.details}}</p>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ['id'],
-  // data() {
-  //   return {
-  //     id: this.$route.params.id
-  //   }
-  // }
+  data() {
+    return {
+      job: null
+    }
+  },
+mounted() {
+  fetch('http://localhost:3000/jobs/' + this.id)
+  .then(res => res.json())
+  .then(data => this.job = data)
+  .catch(err => console.log(err))
+}
 
 }
 </script>
--- ninja-jobs/src/views/jobs/Jobs.vue
+++ ninja-jobs/src/views/jobs/Jobs.vue
@@ -1,10 +1,12 @@
 <template>
 <h1>Jobs</h1>
+<div v-if="jobs.length">
 <div v-for="job in jobs" :key="job.id" class="job" >
 
   <router-link :to="{name: 'JobDetails', params: {id: job.id} }">
     <h2> {{job.title}} </h2>
   </router-link>
+</div>
 </div>
 </template>
 
@@ -12,12 +14,15 @@
 export default {
   data() {
     return {
-      jobs: [
-        {title: 'Ninja UX Designer', id: 1, details: 'lorem'},
-        {title: 'Ninja Web Developer', id: 2, details: 'lorem'},
-        {title: 'Ninja Vue Developer', id: 3, details: 'lorem'}
-      ]
+      jobs: []
+
     }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+    .then(res => res.json())
+    .then(data => this.jobs = data )
+    .catch(err => console.log(err.message))
   }
 
 }
